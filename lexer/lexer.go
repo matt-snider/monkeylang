691b97c6dc2aa73422cb28ea8ff3c8dd68ffaaf7
Matt Snider <matt.snider@alum.utoronto.ca>
2018-04-15 17:25:07 +0200

    lexer: Lex the first few tokens

---
+++ lexer/lexer.go
@@ -0,0 +1,55 @@
+package lexer
+
+import "github.com/matt-snider/monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	tok.Literal = string(l.ch)
+	switch l.ch {
+	case '=':
+		tok.Type = token.ASSIGN
+	case '+':
+		tok.Type = token.PLUS
+	case ',':
+		tok.Type = token.COMMA
+	case ';':
+		tok.Type = token.SEMICOLON
+	case '(':
+		tok.Type = token.LPAREN
+	case ')':
+		tok.Type = token.RPAREN
+	case '{':
+		tok.Type = token.LBRACE
+	case '}':
+		tok.Type = token.RBRACE
+	case 0:
+		tok.Type = token.EOF
+		tok.Literal = ""
+	}
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
