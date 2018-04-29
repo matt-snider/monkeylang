756c746b15d75cf4ec3aba908514c182d5c43832
Matt Snider <matt.snider@alum.utoronto.ca>
2018-04-29 10:37:09 +0200

    ast: Add ExpressionStatement

--- ast/ast.go
+++ ast/ast.go
@@ -65,6 +65,28 @@ func (id *Identifier) String() string {
 }
 
 /**
+ * ExpressionStatement
+ */
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+/**
  * LetStatement
  */
 
